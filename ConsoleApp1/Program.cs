b34d771202f970c34b9174b4f00e3afcb6b0b6e1
BenBo228 <abezrukavyy1104@gmail.com>
2023-11-27 20:23:35 +0300

    Kmq

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,8 +10,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Aleksey GO TO AVTOBYS");
-            Console.WriteLine("Hello Andre GO TO OZON");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hello Aleksey GO TO AVTOBYS");
+                Console.WriteLine("Hello Andre GO TO OZON");
+            }
             //PRO GANSTA REP A.K. ALEKSEY
         }
     }
