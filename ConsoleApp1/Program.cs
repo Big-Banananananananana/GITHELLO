b95c94f316c367c9a6cc5e321ea50513bb84a179
AD-vALEXv <konovalov180305@gmail.com>
2023-11-27 19:59:30 +0300

    Hello

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Aleksey GO TO AVTOBYS");
+            Console.WriteLine("Hello Andre GO TO OZON");
         }
     }
 }
